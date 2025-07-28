eb8f885139a78c5d1f3e1a8a21af6c79aa8a7de7
IlyaIlty <yrchenkovilya@yandex.ru>
2025-07-28 17:05:03 +0300

    добавлен комбо бокс для выбора тегов, поле ентри удаленно

--- Cats.py
+++ Cats.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk
 import requests
 from io import BytesIO
@@ -6,6 +7,7 @@ from io import BytesIO
 #from PyQt5.QtWidgets.QWidget import window
 #from spyder_kernels.utils.iofuncs import load_image
 
+Allowed_tags = ['sleep','play','jump','fight','black','white','siames','cute',]
 
 def load_image(url):
     try:
@@ -21,7 +23,7 @@ def load_image(url):
 
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
     img = load_image(url_tag)
 
@@ -41,11 +43,6 @@ window = Tk()
 window.title('Cats!')
 window.geometry('600x520')
 
-tag_entry = Entry()
-tag_entry.pack()
-
-load_button = Button('загрузить по тэгу', command=open_new_window)
-load_button.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
@@ -57,6 +54,15 @@ file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
 
 url = 'https://cataas.com/cat'
+
+tag_label = Label(text='Выбери тэг')
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button('загрузить по тэгу', command=open_new_window)
+load_button.pack()
 
 window.mainloop()
 
