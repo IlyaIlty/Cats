99b39001db5c4ae5e555aa232385c04a0908a23e
IlyaIlty <yrchenkovilya@yandex.ru>
2025-07-28 16:27:30 +0300

    Создана функция для получения изображения с котиком

--- Cats.py
+++ Cats.py
@@ -6,6 +6,19 @@ from io import BytesIO
 from PyQt5.QtWidgets.QWidget import window
 from spyder_kernels.utils.iofuncs import load_image
 
+
+def loade_image():
+    try:
+        responce = requests.get(url)
+        responce.raise_for_status()
+        image_data = BytesIO(responce.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Проищошла ошибка: {e}')
+        return None
+
+
 window = Tk()
 window.title('Cats!')
 window.geometry('600x400')
