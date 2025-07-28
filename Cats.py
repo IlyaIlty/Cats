62dd472c81cad33a5c4cb3205173e1d38383dfb4
IlyaIlty <yrchenkovilya@yandex.ru>
2025-07-28 16:23:30 +0300

    Создано окно и виджеты

--- Cats.py
+++ Cats.py
@@ -1,1 +1,24 @@
 from tkinter import *
+from PIL import ImageTk
+import requests
+from io import BytesIO
+
+from PyQt5.QtWidgets.QWidget import window
+from spyder_kernels.utils.iofuncs import load_image
+
+window = Tk()
+window.title('Cats!')
+window.geometry('600x400')
+
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
+
